f29925015446a2de2fe6cf8c313aed39848d06da
Manvendra singh <ms1577239@gmail.com>
2026-01-11 19:21:41 +0530

    fix: refactor Home component layout and update syntax examples for clarity

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 import Hero from "@/components/landing/hero";
 import LandingNavBar from "@/components/landing/navbar";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import {
-  ArrowRight,
-  ArrowUpRight,
-  Code,
-  Code2,
-  Code2Icon,
-  GamepadDirectional,
-} from "lucide-react";
+import { ArrowUpRight, Code2, GamepadDirectional, Play } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -18,7 +10,8 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-[1400px] flex-col items-center gap-8 bg-white dark:bg-black">
         <LandingNavBar />
         <Hero />
-        <section className="rounded-4xl p-6 sm:p-10 border w-[90%] mb-10">
+        <section className="relative rounded-4xl p-6 sm:p-10 border w-[90%] mb-10">
+          <div className="bg-muted rounded-full w-16 absolute h-4 top-2 left-1/2 -translate-x-1/2"/>
           <h1 className="text-xl sm:text-4xl font-semibold opacity-60 leading-tight">
             What's new in JSC
           </h1>
@@ -37,27 +30,33 @@ export default function Home() {
                 </Button>
               </Link>
             </div>
-            <Separator orientation="vertical" className="h-full" />
-            <div className="hidden sm:block w-[40%] ml-auto space-y-4">
-              <div className="relative bg-muted w-full rounded-2xl font-mono p-4">
+            <div className="mask-b-from-60% hidden sm:block w-[40%] ml-auto space-y-4 border">
+              <div className="relative bg-muted max-w-full rounded-2xl font-mono p-4 m-4">
                 <p>
                   <span className="text-violet-600 font-medium">var</span>{" "}
-                  developer ={" "}
-                  <span className="text-emerald-500">"Manvendra singh"</span>;
+                  developer = <span className="text-emerald-500">"Manu"</span>;
                 </p>
                 <p>
                   <span className="text-violet-600 font-medium">render</span>{" "}
                   <span className="text-emerald-500">"Hello world "</span> +
                   developer;
                 </p>
+                <Button
+                  variant={"outline"}
+                  size={"icon"}
+                  className="absolute top-2 right-2"
+                >
+                  <Play className="fill-inherit" />
+                </Button>
               </div>
               <div className="w-full h-0.5 bg-muted" />
-              <div className="relative bg-muted w-full rounded-2xl font-mono p-4">
-                <span className="text-fuchsia-500">manvendra@jsc:~$ </span><span className="text-emerald-500">Hello world Manvendra singh</span>
+              <div className="relative bg-muted max-w-full rounded-2xl font-mono p-4 m-4">
+                <span className="text-fuchsia-500">manvendra@jsc:~$ </span>
+                <span className="text-emerald-500">Hello world Manu</span>
               </div>
             </div>
           </div>
-          <div className="flex flex-row-reverse mt-10 sm:mt-8 items-center gap-4 sm:gap-8 w-full">
+          <div className="flex flex-row-reverse mt-10 sm:mt-32 items-center gap-4 sm:gap-8 w-full">
             <GamepadDirectional className="hidden sm:block size-42 bg-muted rounded-full p-2.5 sm:p-4 stroke-primary hover:shadow-xl transition" />
             <div className="flex flex-col gap-2 sm:text-end">
               <span className="text-base sm:text-2xl leading-tight">
