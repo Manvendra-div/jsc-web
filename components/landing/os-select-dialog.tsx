4336f74383b4e63777f639cb74a200e4de8adda4
Manvendra singh <ms1577239@gmail.com>
2026-04-03 22:26:10 +0530

    refactor: adjust spacing in OS selection dialog for improved layout

--- components/landing/os-select-dialog.tsx
+++ components/landing/os-select-dialog.tsx
@@ -22,7 +22,7 @@ export default function OSSelectDialog({ children }: { children: ReactNode }) {
             Select your OS to download the best compatible binaries
           </DialogDescription>
         </DialogHeader>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-2 sm:gap-4">
           <Button
             variant={"secondary"}
             size={"lg"}
@@ -39,7 +39,7 @@ export default function OSSelectDialog({ children }: { children: ReactNode }) {
               />
             </Link>
             <div className="absolute inset-0 bg-accent/60 flex items-center justify-center">
-              <span className="border-2 border-dashed px-4 py-2 bg-background/90 text-muted-foreground">Will be available soon</span>
+              <span className="border-2 border-dashed px-1.5 sm:px-4 py-2 bg-background/90 text-muted-foreground text-xs sm:text-sm">Will be available soon</span>
             </div>
           </Button>
           <Button
