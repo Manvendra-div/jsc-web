480274d23006a15e459f16066ce7792e897608c2
Manvendra singh <ms1577239@gmail.com>
2026-04-03 22:24:34 +0530

    refactor: update layout of hero component and clean up button styles

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -15,23 +15,15 @@ export default function Hero() {
         Designed to explore language design, parsing, and execution from
         scratch.
       </span>
-      <div className="flex items-center gap-2.5">
+      <div className="flex flex-wrap max-w-72 justify-center items-center gap-2.5">
         <OSSelectDialog>
           <Button
             size={"lg"}
-            className="relative before:absolute before:top-0 before:left-0 before:w-full
-     before:h-full before:content-[''] before:opacity-[0.12] before:z-10 before:pointer-events-none
-     before:bg-[url('https://www.ui-layouts.com/noise.gif')] overflow-hidden"
+            className="relative before:absolute before:top-0 before:left-0 before:w-full before:h-full before:content-[''] before:opacity-[0.12] before:z-10 before:pointer-events-none before:bg-[url('https://www.ui-layouts.com/noise.gif')] overflow-hidden"
           >
             Download <Download className="w-4-h-4" />
           </Button>
         </OSSelectDialog>
-        <Button asChild size={"lg"}>
-          <Link href="/docs">
-            Read Docs
-            <BookOpenText className="w-4 h-4" />
-          </Link>
-        </Button>
         <Button asChild variant={"secondary"} size={"lg"}>
           <Link href="/playground">
             Try it <ArrowUpRight />
